78e93fa93a1c98dcba3205a91f4a93c37e4ff889
rereard <rereardany60@gmail.com>
2025-04-07 12:04:29 +0700

    quickfix

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import "leaflet/dist/leaflet.css";
 import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Province from './pages/Province';
+import Survival from './pages/Survival';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const historyVer = localStorage.getItem('ver')
+    if(!historyVer && historyVer !== '0.5'){
+      localStorage.clear()
+      localStorage.setItem('ver', '0.5')
+    }
+  }, []);
+
   return(
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/:provinceName' element={<Province />} />
+      <Route path='/survival' element={<Survival />} />
     </Routes>
   )
 }
