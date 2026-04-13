8e1f6677c46b28f111bb1d8103609430f431a7e6
rereard <rereardany60@gmail.com>
2026-04-13 19:53:28 +0700

    update sitemap

--- generateSitemap.js
+++ generateSitemap.js
@@ -44,6 +44,23 @@ const generateSitemap = () => {
       <changefreq>monthly</changefreq>
     </url>`;
 
+  // add content pages
+  const contentPages = [
+    { path: '/tentang', priority: '0.6' },
+    { path: '/cara-bermain', priority: '0.6' },
+    { path: '/kebijakan-privasi', priority: '0.3' },
+  ]
+
+  contentPages.forEach(({ path, priority }) => {
+    urls += `
+    <url>
+      <loc>${BASE_URL}${path}</loc>
+      <priority>${priority}</priority>
+      <lastmod>${today}</lastmod>
+      <changefreq>monthly</changefreq>
+    </url>`;
+  })
+
   const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     ${urls}
