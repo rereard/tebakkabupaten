925df9482cd514977360ecc70a539bce420a1b4f
rereard <rereardany60@gmail.com>
2026-09-18 18:44:10 +0700

    forgot to include the key in all url

--- src/pages/Province/index.tsx
+++ src/pages/Province/index.tsx
@@ -58,7 +58,7 @@ export default function Province(){
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png'
+        url={import.meta.env.VITE_CARTO_TILE_URL}
         subdomains='abcd'
       />
       {geojsonData && (
--- src/pages/Survival/index.tsx
+++ src/pages/Survival/index.tsx
@@ -187,7 +187,7 @@ export default function Survival(){
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png'
+        url={import.meta.env.VITE_CARTO_TILE_URL}
         subdomains='abcd'
       />
       {geojsonData && (
